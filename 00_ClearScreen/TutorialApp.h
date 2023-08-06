3275bdad2883f9ab259af3109075adf3259713c2
LeeDongWon <ldw9981@gmail.com>
2023-08-07 04:57:45 +0900

    주석 변경

--- 00_ClearScreen/TutorialApp.h
+++ 00_ClearScreen/TutorialApp.h
@@ -10,10 +10,10 @@ public:
 	~TutorialApp();
 
 	// DirectX 변수.
-	ID3D11Device* pDevice = nullptr;
-	ID3D11DeviceContext* pDeviceContext = nullptr;
-	IDXGISwapChain* pSwapChain = nullptr;
-	ID3D11RenderTargetView* pRenderTargetView = nullptr;	
+	ID3D11Device* pDevice = nullptr;						// 디바이스	
+	ID3D11DeviceContext* pDeviceContext = nullptr;			// 디바이스 컨텍스트
+	IDXGISwapChain* pSwapChain = nullptr;					// 스왑체인
+	ID3D11RenderTargetView* pRenderTargetView = nullptr;	// 렌더링 타겟뷰
 
 	virtual bool Initialize(UINT Width, UINT Height);	// 윈도우 정보는 게임 마다 다를수 있으므로 등록,생성,보이기만 한다.
 	virtual void Update();
