7d8cc4b3868ffc9b01caac75910885d67ddeb99b
LeeDongWon <ldw9981@gmail.com>
2023-08-12 03:48:14 +0900

    주석 수정

--- 01_imgui/TutorialApp.h
+++ 01_imgui/TutorialApp.h
@@ -19,12 +19,6 @@ public:
 	IDXGISwapChain* pSwapChain = nullptr;
 	ID3D11RenderTargetView* pRenderTargetView = nullptr;
 
-
-	// Our state
-	bool show_demo_window = true;
-	bool show_another_window = false;
-	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
-
 	Vector4 m_ClearColor = Vector4(0.45f, 0.55f, 0.60f, 1.00f);
 
 
--- 02_RenderingTriangle/TutorialApp.h
+++ 02_RenderingTriangle/TutorialApp.h
@@ -3,31 +3,31 @@
 #include "../Common/GameApp.h"
 
 class TutorialApp :
-    public GameApp
+	public GameApp
 {
 public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
 
 	// DirectX 변수.
-	ID3D11Device*			m_pDevice = nullptr;			// 디바이스	
-	ID3D11DeviceContext*	m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
-	IDXGISwapChain*			m_pSwapChain = nullptr;			// 스왑체인
+	ID3D11Device* m_pDevice = nullptr;			// 디바이스	
+	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
+	IDXGISwapChain* m_pSwapChain = nullptr;			// 스왑체인
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 렌더링 타겟뷰
-	
-	ID3D11VertexShader*		m_pVertexShader = nullptr;		// 정점 셰이더.
-	ID3D11PixelShader*		m_pPixelShader = nullptr;		// 픽셀 셰이더.
-	ID3D11Buffer*			m_pVertexBuffer = nullptr;		// 정점 버퍼.
-	ID3D11InputLayout*		m_pVertexInputLayout = nullptr;	// 입력 레이아웃.
+
+	ID3D11VertexShader* m_pVertexShader = nullptr;		// 정점 셰이더.
+	ID3D11PixelShader* m_pPixelShader = nullptr;		// 픽셀 셰이더.
+	ID3D11Buffer* m_pVertexBuffer = nullptr;		// 정점 버퍼.
+	ID3D11InputLayout* m_pVertexInputLayout = nullptr;	// 입력 레이아웃.
 
 	virtual bool Initialize(UINT Width, UINT Height);
 	virtual void Update();
 	virtual void Render();
 
-	bool InitD3D();			
+	bool InitD3D();
 	void UninitD3D();
 
 	bool InitScene();		// 쉐이더,버텍스,인덱스
-	void UninitScene();		 
+	void UninitScene();
 };
 
