f0da34c52adbb0d5cb3b223da4a7c9710de8242a
LeeDongWon <ldw9981@gmail.com>
2023-08-14 03:24:09 +0900

    설명변경

--- 01_Basics/TutorialApp.h
+++ 01_Basics/TutorialApp.h
@@ -9,12 +9,12 @@ public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
 
-	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스
+	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스 ( 뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.)
 	ID3D11Device* m_pDevice = nullptr;						// 디바이스	
 	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
 	IDXGISwapChain* m_pSwapChain = nullptr;					// 스왑체인
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 렌더링 타겟뷰
-	// 뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.
+	
 
 	virtual bool Initialize(UINT Width, UINT Height);	// 윈도우 정보는 게임 마다 다를수 있으므로 등록,생성,보이기만 한다.
 	virtual void Update();
