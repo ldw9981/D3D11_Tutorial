83f835bfca326f154a99cb49121f89471185e6a9
LeeDongWon <ldw9981@gmail.com>
2023-08-14 02:53:33 +0900

    주석 변경

--- 02_RenderingTriangle/TutorialApp.h
+++ 02_RenderingTriangle/TutorialApp.h
@@ -9,16 +9,13 @@ public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
 
-	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스
+	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스 (  뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.)
 	ID3D11Device* m_pDevice = nullptr;						// 디바이스	
 	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
 	IDXGISwapChain* m_pSwapChain = nullptr;					// 스왑체인
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 렌더링 타겟뷰
-	// 뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.
-
-
+	
 	// 렌더링 파이프라인에 적용하는  객체와 정보
-
 	ID3D11VertexShader* m_pVertexShader = nullptr;		// 정점 셰이더.
 	ID3D11PixelShader* m_pPixelShader = nullptr;		// 픽셀 셰이더.	
 	ID3D11InputLayout* m_pInputLayout = nullptr;	// 입력 레이아웃.
