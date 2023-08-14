7635d23e8fccc981f957a2cd337f473f932ddc78
LeeDongWon <ldw9981@gmail.com>
2023-08-15 03:31:48 +0900

    코드 위치 변경

--- 06_Lighting/TutorialApp.cpp
+++ 06_Lighting/TutorialApp.cpp
@@ -88,15 +88,8 @@ void TutorialApp::Render()
 	vLightDir = XMVector3Transform(vLightDir, mRotate);
 	XMStoreFloat4(&vLightDirs[1], vLightDir);
 
-	//
-	// Clear the back buffer
-	//
-
+	// Clear 
 	m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
-
-	//
-	// Clear the depth buffer to 1.0 (max depth)
-	//
 	m_pDeviceContext->ClearDepthStencilView(m_pDepthStencilView, D3D11_CLEAR_DEPTH, 1.0f, 0);
 
 	//
@@ -113,18 +106,21 @@ void TutorialApp::Render()
 	cb1.vOutputColor = XMFLOAT4(0, 0, 0, 0);
 	m_pDeviceContext->UpdateSubresource(m_pConstantBuffer, 0, nullptr, &cb1, 0, 0);
 
-	//
+	
 	// Render the cube
-	//
+	m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+	m_pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &m_VertexBufferStride, &m_VertexBufferOffset);
+	m_pDeviceContext->IASetInputLayout(m_pInputLayout);
+	m_pDeviceContext->IASetIndexBuffer(m_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
 	m_pDeviceContext->VSSetShader(m_pVertexShader, nullptr, 0);
 	m_pDeviceContext->VSSetConstantBuffers(0, 1, &m_pConstantBuffer);
 	m_pDeviceContext->PSSetShader(m_pPixelShader, nullptr, 0);
 	m_pDeviceContext->PSSetConstantBuffers(0, 1, &m_pConstantBuffer);
+
 	m_pDeviceContext->DrawIndexed(36, 0, 0);
 
-	//
-	// Render each light
-	//
+	
+	// Render each light	
 	for (int m = 0; m < 2; m++)
 	{
 		XMMATRIX mLight = XMMatrixTranslationFromVector(5.0f * XMLoadFloat4(&vLightDirs[m]));
@@ -148,17 +144,14 @@ void TutorialApp::Render()
 
 bool TutorialApp::InitD3D()
 {
-	// 결과값.
-	HRESULT hr;
+	HRESULT hr = 0;	// 결과값.
 
 	// 스왑체인 속성 설정 구조체 생성.
-	DXGI_SWAP_CHAIN_DESC swapDesc;
-	ZeroMemory(&swapDesc, sizeof(DXGI_SWAP_CHAIN_DESC));
+	DXGI_SWAP_CHAIN_DESC swapDesc = {};
 	swapDesc.BufferCount = 1;
 	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapDesc.OutputWindow = m_hWnd;	// 스왑체인 출력할 창 핸들 값.
 	swapDesc.Windowed = true;		// 창 모드 여부 설정.
-	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
 	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	// 백버퍼(텍스처)의 가로/세로 크기 설정.
 	swapDesc.BufferDesc.Width = m_ClientWidth;
@@ -170,34 +163,33 @@ bool TutorialApp::InitD3D()
 	swapDesc.SampleDesc.Count = 1;
 	swapDesc.SampleDesc.Quality = 0;
 
-	// 1. 장치 와 스왑체인 생성.
-	hr = D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, NULL, NULL, NULL,
-		D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice, NULL, &m_pDeviceContext);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
+	UINT creationFlags = 0;
+#ifdef _DEBUG
+	creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
+#endif
+	// 1. 장치 생성.   2.스왑체인 생성. 3.장치 컨텍스트 생성.
+	HR_T(D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, creationFlags, NULL, NULL,
+		D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice, NULL, &m_pDeviceContext));
 
-	// 2. 렌더타겟뷰 생성.
-	// 스왑체인의 내부의 백버퍼를 얻습니다. 
-	ID3D11Texture2D* pBackBufferTexture;
-	hr = m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
+	// 4. 렌더타겟뷰 생성.  (백버퍼를 이용하는 렌더타겟뷰)	
+	ID3D11Texture2D* pBackBufferTexture = nullptr;
+	HR_T(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture));
+	HR_T(m_pDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &m_pRenderTargetView));  // 텍스처는 내부 참조 증가
+	SAFE_RELEASE(pBackBufferTexture);	//외부 참조 카운트를 감소시킨다.
+	// 렌더 타겟을 최종 출력 파이프라인에 바인딩합니다.
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
 
-	// 스왑체인의 백버퍼를 이용하는 렌더타겟뷰를 생성합니다.
-	hr = m_pDevice->CreateRenderTargetView(
-		pBackBufferTexture, NULL, &m_pRenderTargetView);
-	// 렌더타겟뷰를 만들었으므로 백버퍼 텍스처 인터페이스는 더이상 필요하지 않습니다.
-	SAFE_RELEASE(pBackBufferTexture);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
+	//5. 뷰포트 설정.	
+	D3D11_VIEWPORT viewport = {};
+	viewport.TopLeftX = 0;
+	viewport.TopLeftY = 0;
+	viewport.Width = (float)m_ClientWidth;
+	viewport.Height = (float)m_ClientHeight;
+	viewport.MinDepth = 0.0f;
+	viewport.MaxDepth = 1.0f;
+	m_pDeviceContext->RSSetViewports(1, &viewport);
 
-	// Create depth stencil texture
+	//6. 뎊스&스텐실 뷰 생성
 	D3D11_TEXTURE2D_DESC descDepth = {};
 	descDepth.Width = m_ClientWidth;
 	descDepth.Height = m_ClientHeight;
@@ -211,177 +203,42 @@ bool TutorialApp::InitD3D()
 	descDepth.CPUAccessFlags = 0;
 	descDepth.MiscFlags = 0;
 
-	ID3D11Texture2D *pDepthStencilTexture = nullptr;
-	hr = m_pDevice->CreateTexture2D(&descDepth, nullptr, &pDepthStencilTexture);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
+	ID3D11Texture2D* textureDepthStencil = nullptr;
+	HR_T(m_pDevice->CreateTexture2D(&descDepth, nullptr, &textureDepthStencil));
 
 	// Create the depth stencil view
 	D3D11_DEPTH_STENCIL_VIEW_DESC descDSV = {};
 	descDSV.Format = descDepth.Format;
 	descDSV.ViewDimension = D3D11_DSV_DIMENSION_TEXTURE2D;
 	descDSV.Texture2D.MipSlice = 0;
-	hr = m_pDevice->CreateDepthStencilView(pDepthStencilTexture, &descDSV, &m_pDepthStencilView);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
+	HR_T(m_pDevice->CreateDepthStencilView(textureDepthStencil, &descDSV, &m_pDepthStencilView));
+	SAFE_RELEASE(textureDepthStencil);
 
 	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, m_pDepthStencilView);
 
-	//4. 뷰포트 설정.	
-	D3D11_VIEWPORT viewport;
-	ZeroMemory(&viewport, sizeof(D3D11_VIEWPORT));
-	viewport.TopLeftX = 0;
-	viewport.TopLeftY = 0;
-	viewport.Width = (float)m_ClientWidth;
-	viewport.Height = (float)m_ClientHeight;
-	viewport.MinDepth = 0.0f;
-	viewport.MaxDepth = 1.0f;
-
-	// 뷰포트 설정.
-	m_pDeviceContext->RSSetViewports(1, &viewport);
 	return true;
 }
 
 void TutorialApp::UninitD3D()
 {
-	// Cleanup DirectX
 	SAFE_RELEASE(m_pDevice);
 	SAFE_RELEASE(m_pDeviceContext);
 	SAFE_RELEASE(m_pSwapChain);
 	SAFE_RELEASE(m_pRenderTargetView);
 }
 
+// 1. Render() 에서 파이프라인에 바인딩할 버텍스 버퍼및 버퍼 정보 준비
+// 2. Render() 에서 파이프라인에 바인딩할 InputLayout 생성 	
+// 3. Render() 에서 파이프라인에 바인딩할  버텍스 셰이더 생성
+// 4. Render() 에서 파이프라인에 바인딩할 인덱스 버퍼 생성
+// 5. Render() 에서 파이프라인에 바인딩할 픽셀 셰이더 생성
+// 6. Render() 에서 파이프라인에 바인딩할 상수 버퍼 생성
 bool TutorialApp::InitScene()
 {
 	HRESULT hr; // 결과값.
 	ID3D10Blob* errorMessage = nullptr;	 // 에러 메시지를 저장할 버퍼.
 
-	//////////////////////////////////////////////////////////////////////////
-	// 정점 셰이더	 
-	// 1. 정점 셰이더 컴파일해서 정점 셰이더 버퍼에 저장.
-	ID3D10Blob* vertexShaderBuffer = nullptr;
-	hr = D3DCompileFromFile(L"BasicVertexShader.hlsl",	// 셰이더 파일 이름.
-		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
-		"main",	// 시작 함수 이름
-		"vs_4_0", // 정점 셰이더 버전.
-		NULL, NULL,
-		&vertexShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
-		&errorMessage);	// 컴파일 에러 메시지가 저장될 버퍼.
-
-	if (FAILED(hr))
-	{
-		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
-		SAFE_RELEASE(errorMessage);	// 에러 메세지 더이상 필요없음
-		return false;
-	}
-
-	// 2. 정점 셰이더 생성.
-	hr = m_pDevice->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(),
-		vertexShaderBuffer->GetBufferSize(), NULL, &m_pVertexShader);
-
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
-
-	// 입력 레이아웃.
-	D3D11_INPUT_ELEMENT_DESC layout[] =
-	{
-		/*LPCSTR SemanticName;
-		UINT SemanticIndex;
-		DXGI_FORMAT Format;
-		UINT InputSlot;
-		UINT AlignedByteOffset;
-		D3D11_INPUT_CLASSIFICATION InputSlotClass;
-		UINT InstanceDataStepRate;*/
-		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
-	};
-
-	// 입력 레이아웃 생성.
-	hr = m_pDevice->CreateInputLayout(layout, ARRAYSIZE(layout),
-		vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &m_pInputLayout);
-	SAFE_RELEASE(vertexShaderBuffer);
-
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
-
-	// 입력 레이아웃 바인딩.
-	m_pDeviceContext->IASetInputLayout(m_pInputLayout);
-
-
-	//////////////////////////////////////////////////////////////////////////
-	// 픽셀 셰이더 
-	// 1. 컴파일.
-	ID3D10Blob* pixelShaderBuffer = nullptr;
-	hr = D3DCompileFromFile(L"BasicPixelShader.hlsl", // 셰이더 파일 이름.
-		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
-		"main",		// 시작 함수 이름
-		"ps_4_0",	// 정점 셰이더 버전.
-		NULL, NULL,
-		&pixelShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
-		&errorMessage);		// 컴파일 에러 메시지가 저장될 버퍼.
-
-	if (FAILED(hr))
-	{
-		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
-		SAFE_RELEASE(errorMessage);
-		return false;
-	}
-	// 2. 픽셀 셰이더 생성.
-	hr = m_pDevice->CreatePixelShader(
-		pixelShaderBuffer->GetBufferPointer(),
-		pixelShaderBuffer->GetBufferSize(), NULL, &m_pPixelShader);
-
-	SAFE_RELEASE(pixelShaderBuffer);
-
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
-
-	//////////////////////////////////////////////////////////////////////////
-	// Solid 픽셀 셰이더 
-	// 1. 컴파일.
-	hr = D3DCompileFromFile(L"SolidPixelShader.hlsl", // 셰이더 파일 이름.
-		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
-		"main",		// 시작 함수 이름
-		"ps_4_0",	// 정점 셰이더 버전.
-		NULL, NULL,
-		&pixelShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
-		&errorMessage);		// 컴파일 에러 메시지가 저장될 버퍼.
-
-	if (FAILED(hr))
-	{
-		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
-		SAFE_RELEASE(errorMessage);
-		return false;
-	}
-	// 2. 픽셀 셰이더 생성.
-	hr = m_pDevice->CreatePixelShader(
-		pixelShaderBuffer->GetBufferPointer(),
-		pixelShaderBuffer->GetBufferSize(), NULL, &m_pPixelShaderSolid);
-
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
-		return false;
-	}
-
-	//3. 픽셀 셰이더 설정.
-	m_pDeviceContext->PSSetShader(m_pPixelShader, NULL, NULL);
-
-
-
-
-
-	//4. 버텍스 버퍼 
-	// 버텍스 데이터(배열) 생성.
+	// 1. Render() 에서 파이프라인에 바인딩할 버텍스 버퍼및 버퍼 정보 준비
 	// Local or Object or Model Space
 	Vertex vertices[] =
 	{
@@ -422,72 +279,111 @@ bool TutorialApp::InitScene()
 	bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.CPUAccessFlags = 0;
-	
+
 	D3D11_SUBRESOURCE_DATA vbData = {};
 	vbData.pSysMem = vertices;
-	hr = m_pDevice->CreateBuffer(&bd, &vbData, &m_pVertexBuffer);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
+	HR_T( m_pDevice->CreateBuffer(&bd, &vbData, &m_pVertexBuffer));
+	
+	// 버텍스 버퍼 바인딩.
+	m_VertexBufferStride = sizeof(Vertex);
+	m_VertexBufferOffset = 0;
+
+	
+	// 2. Render() 에서 파이프라인에 바인딩할 InputLayout 생성 	
+	D3D11_INPUT_ELEMENT_DESC layout[] =
+	{
+		{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+		{ "NORMAL", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0 },
+	};
+
+	ID3D10Blob* vertexShaderBuffer = nullptr;
+	hr = D3DCompileFromFile(L"BasicVertexShader.hlsl",	// 셰이더 파일 이름.
+		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"main",	// 시작 함수 이름
+		"vs_4_0", // 정점 셰이더 버전.
+		NULL, NULL,
+		&vertexShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
+		&errorMessage);	// 컴파일 에러 메시지가 저장될 버퍼.
+
+	if (FAILED(hr))
+	{
+		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
+		SAFE_RELEASE(errorMessage);	// 에러 메세지 더이상 필요없음
 		return false;
 	}
 
-	// 버텍스 버퍼 바인딩.
-	UINT stride = sizeof(Vertex);
-	UINT offset = 0;
-	m_pDeviceContext->IASetVertexBuffers(0, 1, &m_pVertexBuffer, &stride, &offset);
+	HR_T( m_pDevice->CreateInputLayout(layout, ARRAYSIZE(layout),
+		vertexShaderBuffer->GetBufferPointer(), vertexShaderBuffer->GetBufferSize(), &m_pInputLayout));
+	
+	// 3. Render() 에서 파이프라인에 바인딩할  버텍스 셰이더 생성
+	HR_T(m_pDevice->CreateVertexShader(vertexShaderBuffer->GetBufferPointer(),
+		vertexShaderBuffer->GetBufferSize(), NULL, &m_pVertexShader));
 
+	SAFE_RELEASE(vertexShaderBuffer);
 
-	//5. 인덱스 버퍼
-	// 인덱스 버퍼 설정.
+	 
+	// 4. Render() 에서 파이프라인에 바인딩할 인덱스 버퍼 생성
 	WORD indices[] =
 	{
-		3,1,0,
-		2,1,3,
-
-		6,4,5,
-		7,4,6,
-
-		11,9,8,
-		10,9,11,
-
-		14,12,13,
-		15,12,14,
-
-		19,17,16,
-		18,17,19,
-
-		22,20,21,
-		23,20,22
+		3,1,0, 2,1,3,
+		6,4,5, 7,4,6,
+		11,9,8, 10,9,11,
+		14,12,13, 15,12,14,
+		19,17,16, 18,17,19,
+		22,20,21, 23,20,22
 	};
 
 	// 인덱스 개수 저장.
 	m_nIndices = ARRAYSIZE(indices);
 
-	bd.ByteWidth = sizeof(DWORD) * ARRAYSIZE(indices);
+	bd = {};
+	bd.ByteWidth = sizeof(WORD) * ARRAYSIZE(indices);
 	bd.BindFlags = D3D11_BIND_INDEX_BUFFER;
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.CPUAccessFlags = 0;
-
-	D3D11_SUBRESOURCE_DATA ibData;
-	ZeroMemory(&ibData, sizeof(D3D11_SUBRESOURCE_DATA));
+	D3D11_SUBRESOURCE_DATA ibData = {};
 	ibData.pSysMem = indices;
+	HR_T(m_pDevice->CreateBuffer(&bd, &ibData, &m_pIndexBuffer));
 
-	// 인덱스 버퍼 생성.
-	hr = m_pDevice->CreateBuffer(&bd, &ibData, &m_pIndexBuffer);
-	if (FAILED(hr)) {
-		LOG_ERROR(L"%s", GetComErrorString(hr));
+	// 5. Render() 에서 파이프라인에 바인딩할 픽셀 셰이더 생성
+	ID3D10Blob* pixelShaderBuffer = nullptr;
+	hr = D3DCompileFromFile(L"BasicPixelShader.hlsl", // 셰이더 파일 이름.
+		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"main",		// 시작 함수 이름
+		"ps_4_0",	// 정점 셰이더 버전.
+		NULL, NULL,
+		&pixelShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
+		&errorMessage);		// 컴파일 에러 메시지가 저장될 버퍼.
+
+	if (FAILED(hr))
+	{
+		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
+		SAFE_RELEASE(errorMessage);
 		return false;
 	}
+	HR_T(m_pDevice->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(),
+		pixelShaderBuffer->GetBufferSize(), NULL, &m_pPixelShader));
+	SAFE_RELEASE(pixelShaderBuffer);
 
-	// 인덱스 버퍼 바인딩(binding).
-	m_pDeviceContext->IASetIndexBuffer(m_pIndexBuffer, DXGI_FORMAT_R16_UINT, 0);
 
+	hr = D3DCompileFromFile(L"SolidPixelShader.hlsl", // 셰이더 파일 이름.
+		NULL, D3D_COMPILE_STANDARD_FILE_INCLUDE,
+		"main",		// 시작 함수 이름
+		"ps_4_0",	// 정점 셰이더 버전.
+		NULL, NULL,
+		&pixelShaderBuffer, // 컴파일된 셰이더 코드가 저장될 버퍼.
+		&errorMessage);		// 컴파일 에러 메시지가 저장될 버퍼.
+	if (FAILED(hr))
+	{
+		MessageBoxA(m_hWnd, (char*)errorMessage->GetBufferPointer(), "오류.", MB_OK);
+		SAFE_RELEASE(errorMessage);
+		return false;
+	}
+	HR_T(m_pDevice->CreatePixelShader(pixelShaderBuffer->GetBufferPointer(),
+		pixelShaderBuffer->GetBufferSize(), NULL, &m_pPixelShaderSolid));
+	SAFE_RELEASE(pixelShaderBuffer);
 
-
-	// 정점을 이어서 그릴 방식 설정.
-	m_pDeviceContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-
-	// Create the constant buffer
+	// 6. Render() 에서 파이프라인에 바인딩할 상수 버퍼 생성	
 	bd.Usage = D3D11_USAGE_DEFAULT;
 	bd.ByteWidth = sizeof(ConstantBuffer);
 	bd.BindFlags = D3D11_BIND_CONSTANT_BUFFER;
@@ -498,20 +394,13 @@ bool TutorialApp::InitScene()
 		return false;
 	}
 
-	// Initialize the world matrix
+	// 초기값설정
 	m_World = XMMatrixIdentity();
-
-
-	// Initialize the view matrix
 	XMVECTOR Eye = XMVectorSet(0.0f, 4.0f, -10.0f, 0.0f);
 	XMVECTOR At = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
 	XMVECTOR Up = XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f);
-
 	m_View = XMMatrixLookAtLH(Eye, At, Up);
-
-	// Initialize the projection matrix
 	m_Projection = XMMatrixPerspectiveFovLH(XM_PIDIV4, m_ClientWidth / (FLOAT)m_ClientHeight, 0.01f, 100.0f);
-
 	return true;
 }
 
