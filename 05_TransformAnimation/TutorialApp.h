e71e67f60353b048382a98294d807b86bfaeb061
LeeDongWon <ldw9981@gmail.com>
2023-08-15 02:32:43 +0900

    주석 정리

--- 05_TransformAnimation/TutorialApp.h
+++ 05_TransformAnimation/TutorialApp.h
@@ -14,8 +14,8 @@ class TutorialApp :
 public:
 	TutorialApp(HINSTANCE hInstance);
 	~TutorialApp();
-	
-	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스 (  뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.)
+
+	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스 
 	ID3D11Device* m_pDevice = nullptr;						// 디바이스	
 	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
 	IDXGISwapChain* m_pSwapChain = nullptr;					// 스왑체인
@@ -25,13 +25,13 @@ public:
 	// 렌더링 파이프라인에 적용하는  객체와 정보
 	ID3D11VertexShader* m_pVertexShader = nullptr;		// 정점 셰이더.
 	ID3D11PixelShader* m_pPixelShader = nullptr;		// 픽셀 셰이더.	
-	ID3D11InputLayout* m_pInputLayout = nullptr;	// 입력 레이아웃.
-	ID3D11Buffer* m_pVertexBuffer = nullptr;		// 버텍스 버퍼.
-	UINT m_VertexBufferStride = 0;		// 버텍스 하나의 크기.
-	UINT m_VertexBufferOffset = 0;		// 버텍스 버퍼의 오프셋.
-	ID3D11Buffer* m_pIndexBuffer = nullptr;		// 버텍스 버퍼.
-	int m_nIndices = 0;							// 인덱스 개수.
-	ID3D11Buffer* m_pConstantBuffer = nullptr;	// 상수 버퍼.
+	ID3D11InputLayout* m_pInputLayout = nullptr;		// 입력 레이아웃.
+	ID3D11Buffer* m_pVertexBuffer = nullptr;			// 버텍스 버퍼.
+	UINT m_VertexBufferStride = 0;						// 버텍스 하나의 크기.
+	UINT m_VertexBufferOffset = 0;						// 버텍스 버퍼의 오프셋.
+	ID3D11Buffer* m_pIndexBuffer = nullptr;				// 버텍스 버퍼.
+	int m_nIndices = 0;									// 인덱스 개수.
+	ID3D11Buffer* m_pConstantBuffer = nullptr;			// 상수 버퍼.
 
 	Matrix                m_World1;				// 월드좌표계 공간으로 변환을 위한 행렬.
 	Matrix                m_World2;				// 월드좌표계 공간으로 변환을 위한 행렬.
