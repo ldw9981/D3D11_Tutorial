84249ea6a30025f3f934908298bd6ed13d349031
LeeDongWon <ldw9981@gmail.com>
2023-08-22 00:59:23 +0900

    제거

--- 02_Draw/BasicPixelShader.hlsl
+++
@@ -1,5 +0,0 @@
-// «»ºø ºŒ¿Ã¥ı(Ω¶¿Ã¥ı/ºŒ¿Ã¥ı).
-float4 main(float4 input : POSITION) : SV_TARGET
-{
-    return float4(1.0f, 0.0f, 0.0f, 1.0f);
-}
--- 02_Draw/BasicVertexShader.hlsl
+++
@@ -1,5 +0,0 @@
-// 정점 셰이더.
-float4 main(float4 pos : POSITION) : SV_POSITION
-{
-	return pos;
-}
