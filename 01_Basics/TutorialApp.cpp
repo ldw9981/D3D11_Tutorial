d248d27f4f494b66ee5a05059244a057a16c0a3e
LeeDongWon <ldw9981@gmail.com>
2023-08-30 21:08:33 +0900

    Flip모드 사용예 추가

--- 01_Basics/TutorialApp.cpp
+++ 01_Basics/TutorialApp.cpp
@@ -6,6 +6,8 @@
 
 using namespace DirectX::SimpleMath;
 
+#define USE_FLIPMODE 0			//경고 메세지를 없애려면 Flip 모드를 사용한다.
+
 TutorialApp::TutorialApp(HINSTANCE hInstance)
 	:GameApp(hInstance)
 {
@@ -34,6 +36,10 @@ void TutorialApp::Update()
 
 void TutorialApp::Render()
 {
+#if USE_FLIPMODE==1
+	// Flip모드에서는 매프레임 설정 필요
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
+#endif
 	Color color(0.0f, 0.5f, 0.5f, 1.0f);
 	// 화면 칠하기.
 	m_pDeviceContext->ClearRenderTargetView(m_pRenderTargetView, color);
@@ -49,7 +55,13 @@ bool TutorialApp::InitD3D()
 
 	// 스왑체인 속성 설정 구조체 생성.
 	DXGI_SWAP_CHAIN_DESC swapDesc = {};
+#if USE_FLIPMODE==1
+	swapDesc.BufferCount = 2;
+	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+#else
 	swapDesc.BufferCount = 1;
+	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;	//기본값 0
+#endif
 	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	swapDesc.OutputWindow = m_hWnd;	// 스왑체인 출력할 창 핸들 값.
 	swapDesc.Windowed = true;		// 창 모드 여부 설정.
@@ -63,6 +75,7 @@ bool TutorialApp::InitD3D()
 	// 샘플링 관련 설정.
 	swapDesc.SampleDesc.Count = 1;
 	swapDesc.SampleDesc.Quality = 0;
+	 
 
 	UINT creationFlags = 0;
 #ifdef _DEBUG
@@ -78,8 +91,11 @@ bool TutorialApp::InitD3D()
 	HR_T( m_pDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &m_pRenderTargetView) );  // 텍스처는 내부 참조 증가
 	SAFE_RELEASE(pBackBufferTexture);	//외부 참조 카운트를 감소시킨다.
 
+#if USE_FLIPMODE==0
 	// 렌더 타겟을 최종 출력 파이프라인에 바인딩합니다.
+	// FlipMode가 아닐때는 최초 한번만 설정하면 된다.
 	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
+#endif
 	return true;
 }
 
