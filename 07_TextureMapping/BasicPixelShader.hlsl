3fa6fdddfa79ec37d4ca77d8df69400b8cbf59c6
LeeDongWon <ldw9981@gmail.com>
2024-09-02 15:10:45 +0900

    README.rm 추가, 사용안하는 프로젝트제거

--- 07_TextureMapping/BasicPixelShader.hlsl
+++
@@ -1,9 +0,0 @@
-#include "Shared.fxh"
-
-//--------------------------------------------------------------------------------------
-// Pixel Shader
-//--------------------------------------------------------------------------------------
-float4 main(PS_INPUT input) : SV_Target
-{
-    return txDiffuse.Sample(samLinear, input.Tex);
-}
