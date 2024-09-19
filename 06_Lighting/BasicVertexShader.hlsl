2723a86ee0cd4eb2b772945e8db6afdaf1c94c8c
LeeDongWon <ldw9981@gmail.com>
2024-09-19 18:03:05 +0900

    설졍 수정

--- 06_Lighting/BasicVertexShader.hlsl
+++ 06_Lighting/BasicVertexShader.hlsl
@@ -10,7 +10,7 @@ PS_INPUT main(VS_INPUT input)
     output.Pos = mul(output.Pos, View);
     output.Pos = mul(output.Pos, Projection);
     
-    // 이 코드는 World Matrix에 스케일 또는 이동행렬이 적용되어 있으면 올바르게 Normal을 계산하지 않는다.
+    // 이 코드는 World Matrix에 스케일 또는 이동행렬이 적용되어 있으면 크기가 1이 아닌 벡터가 될수있다.
     //output.Norm = mul(float4(input.Norm, 1), World).xyz;    
    
     // World Matrix에서 이동성분을 제외하고 적용하며,  scale 있을수 있으므로 normalize 사용한다.
