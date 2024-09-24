15be67a9f394b27a3d43e7fb7f00c4278e0ff48c
LeeDongWon <ldw9981@gmail.com>
2024-09-24 16:33:41 +0900

    기본 위치 변경

--- Common/Camera.h
+++ Common/Camera.h
@@ -10,10 +10,10 @@ class Camera
 public:
 
 	Vector3 m_Rotation;
-	Vector3 m_Position = { 0,0,-100 } ;
+	Vector3 m_Position = { 0,0,-30 } ;
 	Matrix m_World;
 	Vector3 m_InputVector;
-	float m_Speed = 50.0f;
+	float m_Speed = 20.0f;
 
 	Vector3 GetForward();
 	Vector3 GetRight();
