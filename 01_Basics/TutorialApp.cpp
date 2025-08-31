5e9f6450d187bd7ae604a160c95157ae39386e7f
ldw9981 <ldw9981@gmail.com>
2025-09-01 00:15:33 +0900

    스왑체인도 디버그 메세지가 나오도록 변경

--- 01_Basics/TutorialApp.cpp
+++ 01_Basics/TutorialApp.cpp
@@ -1,12 +1,18 @@
 #include "TutorialApp.h"
 #include "../Common/Helper.h"
+
 #include <directxtk/simplemath.h>
+#include <dxgidebug.h>
+#include <dxgi1_3.h>
+#include <wrl/client.h>
 
 #pragma comment (lib, "d3d11.lib")
+#pragma comment(lib,"dxgi.lib")
 
 using namespace DirectX::SimpleMath;
+using Microsoft::WRL::ComPtr;
 
-#define USE_FLIPMODE 0			//경고 메세지를 없애려면 Flip 모드를 사용한다.
+#define USE_FLIPMODE 1			//경고 메세지를 없애려면 Flip 모드를 사용한다.
 
 TutorialApp::TutorialApp(HINSTANCE hInstance)
 	:GameApp(hInstance)
@@ -48,54 +54,84 @@ void TutorialApp::Render()
 	m_pSwapChain->Present(0, 0);
 }
 
+
 bool TutorialApp::InitD3D()
 {
-	// 결과값.
-	HRESULT hr=0;
+	HRESULT hr = 0;
 
-	// 스왑체인 속성 설정 구조체 생성.
-	DXGI_SWAP_CHAIN_DESC swapDesc = {};
-#if USE_FLIPMODE==1
-	swapDesc.BufferCount = 2;
-	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
-#else
-	swapDesc.BufferCount = 1;
-	swapDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;	//기본값 0
-#endif
-	swapDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-	swapDesc.OutputWindow = m_hWnd;	// 스왑체인 출력할 창 핸들 값.
-	swapDesc.Windowed = true;		// 창 모드 여부 설정.
-	swapDesc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-	// 백버퍼(텍스처)의 가로/세로 크기 설정.
-	swapDesc.BufferDesc.Width = m_ClientWidth;
-	swapDesc.BufferDesc.Height = m_ClientHeight;
-	// 화면 주사율 설정.
-	swapDesc.BufferDesc.RefreshRate.Numerator = 60;
-	swapDesc.BufferDesc.RefreshRate.Denominator = 1;
-	// 샘플링 관련 설정.
-	swapDesc.SampleDesc.Count = 1;
-	swapDesc.SampleDesc.Quality = 0;
-	 
-
-	UINT creationFlags = 0;
+	// 1. D3D11 Device,DeviceContext 생성
+	UINT creationFlags = D3D11_CREATE_DEVICE_BGRA_SUPPORT;
 #ifdef _DEBUG
 	creationFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
-	// 1. 장치 생성.   2.스왑체인 생성. 3.장치 컨텍스트 생성.
-	HR_T( D3D11CreateDeviceAndSwapChain(NULL, D3D_DRIVER_TYPE_HARDWARE, NULL, creationFlags, NULL, NULL,
-		D3D11_SDK_VERSION, &swapDesc, &m_pSwapChain, &m_pDevice, NULL, &m_pDeviceContext));
-	
-	// 4. 렌더타겟뷰 생성.  (백버퍼를 이용하는 렌더타겟뷰)	
-	ID3D11Texture2D* pBackBufferTexture=nullptr;
-	HR_T( m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture) );		
-	HR_T( m_pDevice->CreateRenderTargetView(pBackBufferTexture, NULL, &m_pRenderTargetView) );  // 텍스처는 내부 참조 증가
-	SAFE_RELEASE(pBackBufferTexture);	//외부 참조 카운트를 감소시킨다.
+	// 그래픽 카드 하드웨어의 스펙으로 호환되는 가장 높은 DirectX 기능레벨로 생성하여 드라이버가 작동 한다.
+	// 인터페이스는 Direc3D11 이지만 GPU드라이버는 D3D12 드라이버가 작동할수도 있다.
+	D3D_FEATURE_LEVEL featureLevels[] = { // index 0부터 순서대로 시도한다.
+		D3D_FEATURE_LEVEL_12_2,D3D_FEATURE_LEVEL_12_1,D3D_FEATURE_LEVEL_12_0,D3D_FEATURE_LEVEL_11_1,D3D_FEATURE_LEVEL_11_0
+	};
+	D3D_FEATURE_LEVEL actualFeatureLevel; // 최종 피처 레벨을 저장할 변수
 
-#if USE_FLIPMODE==0
-	// 렌더 타겟을 최종 출력 파이프라인에 바인딩합니다.
-	// FlipMode가 아닐때는 최초 한번만 설정하면 된다.
-	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, NULL);
+	HR_T(D3D11CreateDevice(
+		nullptr,
+		D3D_DRIVER_TYPE_HARDWARE,
+		0,
+		creationFlags,
+		featureLevels,
+		ARRAYSIZE(featureLevels),
+		D3D11_SDK_VERSION,
+		&m_pDevice,
+		&actualFeatureLevel,
+		&m_pDeviceContext
+	));
+
+	// 2. 스왑체인 생성을 위한 DXGI Factory 생성
+	UINT dxgiFactoryFlags = 0;
+#ifdef _DEBUG
+	dxgiFactoryFlags |= DXGI_CREATE_FACTORY_DEBUG;
 #endif
+
+	ComPtr<IDXGIFactory2> pFactory;
+	HR_T(CreateDXGIFactory2(dxgiFactoryFlags, IID_PPV_ARGS(&pFactory)));
+
+	DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+#if USE_FLIPMODE==1
+	swapChainDesc.BufferCount = 2;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+#else
+	swapChainDesc.BufferCount = 1;
+	swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+#endif
+	swapChainDesc.Width = m_ClientWidth;
+	swapChainDesc.Height = m_ClientHeight;
+	// 하나의 픽셀이 채널 RGBA 각 8비트 형식으로 표현되며 
+	// Unsigned Normalized Integer 8비트 정수(0~255)단계를 부동소수점으로 정규화한 0.0~1.0으로 매핑하여 표현한다.
+	swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT; // 스왑 체인의 백 버퍼가 렌더링 파이프라인의 최종 출력 대상으로 사용
+	swapChainDesc.SampleDesc.Count = 1;  // 멀티샘플링 사용 안함
+	swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_IGNORE; // Recommended for flip models
+	swapChainDesc.Stereo = FALSE;  // 스테레오 3D 렌더링을 비활성화
+	swapChainDesc.Flags = DXGI_SWAP_CHAIN_FLAG_ALLOW_MODE_SWITCH; // 전체 화면 전환을 허용
+	swapChainDesc.Scaling = DXGI_SCALING_NONE; //  창의 크기와 백 버퍼의 크기가 다를 때. 백버퍼 크기에 맞게 스케일링 하지 않는다.
+		
+	HR_T(pFactory->CreateSwapChainForHwnd(
+		m_pDevice,
+		m_hWnd,
+		&swapChainDesc,
+		nullptr,
+		nullptr,
+		&m_pSwapChain
+	));
+
+	// 3. 렌더타겟 뷰 생성.  (백버퍼를 텍스처를 이용하는 렌더타겟뷰) 
+	//  렌더 타겟 뷰는 "여기다가 그림을 그려라"라고 GPU에게 알려주는 역할을 하는 객체.
+	ComPtr<ID3D11Texture2D> pBackBufferTexture;
+	HR_T(m_pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), (void**)&pBackBufferTexture));
+	HR_T(m_pDevice->CreateRenderTargetView(pBackBufferTexture.Get(), nullptr, &m_pRenderTargetView));
+
+#if !USE_FLIPMODE
+	m_pDeviceContext->OMSetRenderTargets(1, &m_pRenderTargetView, nullptr);
+#endif
+
 	return true;
 }
 
--- 01_Basics/TutorialApp.h
+++ 01_Basics/TutorialApp.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <d3d11.h>
+
 #include "../Common/GameApp.h"
 
 class TutorialApp :
@@ -12,7 +13,7 @@ public:
 	// 렌더링 파이프라인을 구성하는 필수 객체의 인터페이스 ( 뎊스 스텐실 뷰도 있지만 아직 사용하지 않는다.)
 	ID3D11Device* m_pDevice = nullptr;						// 디바이스	
 	ID3D11DeviceContext* m_pDeviceContext = nullptr;		// 즉시 디바이스 컨텍스트
-	IDXGISwapChain* m_pSwapChain = nullptr;					// 스왑체인
+	IDXGISwapChain1* m_pSwapChain = nullptr;					// 스왑체인
 	ID3D11RenderTargetView* m_pRenderTargetView = nullptr;	// 렌더링 타겟뷰
 	
 
