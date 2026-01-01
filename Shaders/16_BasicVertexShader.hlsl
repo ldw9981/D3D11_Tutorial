df06721ca42bca194e705aec3eedead012915206
ldw9981 <ldw9981@gmail.com>
2026-01-01 15:05:54 +0900

    HDR톤맵핑 수정

--- Shaders/16_BasicVertexShader.hlsl
+++ Shaders/16_BasicVertexShader.hlsl
@@ -9,8 +9,7 @@ PS_INPUT_BASIC main(VS_INPUT_BASIC input)
     output.position = mul(input.position, World);
     output.position = mul(output.position, View);
     output.position = mul(output.position, Projection);
-
-    // World Matrix에서 이동성분을 제외하고 적용하며,  scale 있을수 있으므로 normalize 사용한다.
+    
     output.normal = normalize(mul(input.normal, (float3x3) World)); 
     return output;
 }
