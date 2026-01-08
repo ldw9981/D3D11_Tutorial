ee93c5a8cd81c5727812a33d6ed6020d1a561216
LeeDongWon <ldw9981@gmail.com>
2026-01-08 13:58:40 +0900

    5개적용

--- 15_DeferredRendering/TutorialApp.h
+++ 15_DeferredRendering/TutorialApp.h
@@ -99,7 +99,7 @@ public:
 		float radius;
 	};
 	PointLightData m_PointLights[MAX_POINT_LIGHTS];
-	int m_ActiveLightCount = 1;  // Number of lights to actually render (adjustable)
+	int m_ActiveLightCount = 5;  // Number of lights to actually render (adjustable)
 	float m_GlobalLightRadiusScale = 1.0f;
 
 
